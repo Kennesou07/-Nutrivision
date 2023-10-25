7153a5dc19a16394f2620d3ba712a0708256174f
Kennesou07 <kaineth26@gmail.com>
2023-10-25 22:55:36 +0800

    Google Sign In Update in Login

---
+++ app/src/main/java/com/capstone/nutrivision/ForgotPass.java
@@ -0,0 +1,12 @@
+package com.capstone.nutrivision;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ForgotPass extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgotpassword);
+    }
+}
